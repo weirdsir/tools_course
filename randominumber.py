1410d719c86172c00d8abc45c4c5af08f7ee2390
Onni-Petteri Rantanen <onnin2003@gmail.com>
2024-04-08 17:55:38 +0300

    Capitalise letters, add more numbers

--- randominumber.py
+++ randominumber.py
@@ -1,3 +1,7 @@
 import random
 
-print("heres your number", random.randrange(1,100))
+rand_list=[]
+n=10
+for i in range(n):
+    rand_list.append(random.randint(2,2000))
+print("Here is your list on numbers! ", rand_list)
