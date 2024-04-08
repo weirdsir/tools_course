0be7814f25b2b14c00df8227ccd32e58da13f976
Danila Karpov <gpdanila@gmail.com>
2024-04-08 17:31:36 +0300

    Add color iteration

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+def print_color(text, color):
+    print("\033[{}m{}\033[0m".format(color, text), end='')
+
 alphabet = []
+colors = ["31", "32", "33", "34", "35", "36", "37", "91", "92", "93", "94", "95", "96", "97"]
+color_index = 0
+
 for letter in range(ord('a'), ord('z')+1):
     alphabet.append(chr(letter))
+    print_color(chr(letter), colors[color_index])
+    color_index = (color_index + 1) % len(colors)
 
-print("Alphabet:", alphabet)
+print("\nAlphabet:", alphabet)
