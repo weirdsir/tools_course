5a0b224b15622df18fd0f0ed7c6558979ff4f64a
Danila Karpov <gpdanila@gmail.com>
2024-04-08 17:56:55 +0300

    Add interface

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-def print_color(text, color):
-    print("\033[{}m{}\033[0m".format(color, text), end='')
+import tkinter as tk
 
-alphabet = []
-colors = ["31", "32", "33", "34", "35", "36", "37", "91", "92", "93", "94", "95", "96", "97"]
-color_index = 0
+class AlphabetApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Alphabet Colors")
+        self.geometry("400x200")
 
-for letter in range(ord('a'), ord('z')+1):
-    alphabet.append(chr(letter))
-    print_color(chr(letter), colors[color_index])
-    color_index = (color_index + 1) % len(colors)
+        self.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.colors = ['#FF5733', '#C70039', '#900C3F', '#581845', '#5D6D7E', '#34495E', '#2E4053', '#212F3C',
+                       '#566573', '#1F618D', '#2874A6', '#2980B9', '#5499C7', '#7FB3D5', '#AED6F1', '#EBF5FB',
+                       '#1ABC9C', '#17A589', '#148F77', '#117A65', '#0E6655', '#0B5345', '#0A3E23', '#117A65',
+                       '#1E8449', '#239B56']
 
-print("\nAlphabet:", alphabet)
+        self.create_alphabet_labels()
+
+    def create_alphabet_labels(self):
+        for i, letter in enumerate(self.alphabet):
+            tk.Label(self, text=letter, font=("Comic Sans MS", 18), fg=self.colors[i]).grid(row=0, column=i, padx=5)
+
+if __name__ == "__main__":
+    app = AlphabetApp()
+    app.mainloop()
