372957e1c18b62cec3a9fe965a6ffc04e821c476
Onni-Petteri Rantanen <onnin2003@gmail.com>
2024-04-08 17:34:01 +0300

    random number generator
    adding random number generator

--- randominumber.py
+++ randominumber.py
@@ -1,7 +1,11 @@
 import random
 
+
 rand_list=[]
 n=10
 for i in range(n):
     rand_list.append(random.randint(2,2000))
 print("Here is your list on numbers! ", rand_list)
+
+print("heres your number", random.randrange(1,100))
+
