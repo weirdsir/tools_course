7eec5de2ce75ff4482b0e7fe17f88ba6886c2138
Rin <67466959+weirdsir@users.noreply.github.com>
2024-04-11 22:12:13 +0300

    Create guess_number.py
    we have a random number generator 
    now its a guessing game

---
+++ guess_number.py
@@ -0,0 +1,20 @@
+import random
+
+def guessing_game():
+    print("guess a number")
+    secret_number = random.randint(1, 100)
+    attempts = 0
+
+    while True:
+        guess = int(input("enter guess between 1 and 100"))
+        attempts += 1
+
+        if guess < secret_number:
+            print("too low:( try again")
+        elif guess > secret_number:
+            print("too high:( try again")
+        else:
+            print(f"congratulations!!! you guessed the number in {attempts} attempts!")
+            break
+
+guessing_game()
