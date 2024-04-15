bdc2b890170f3e9acda6d52e2bd17e7c02449e48
dkrpv <35373444+dkrpv@users.noreply.github.com>
2024-04-15 19:40:42 +0300

    Update alphabet
    * Update alphabet
    
    * Add missing alphabet letters

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class AlphabetApp(tk.Tk):
         self.title("Alphabet Colors")
         self.geometry("400x200")
 
-        self.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ"
         self.colors = ['#FF5733', '#C70039', '#900C3F', '#581845', '#5D6D7E', '#34495E', '#2E4053', '#212F3C',
                        '#566573', '#1F618D', '#2874A6', '#2980B9', '#5499C7', '#7FB3D5', '#AED6F1', '#EBF5FB',
                        '#1ABC9C', '#17A589', '#148F77', '#117A65', '#0E6655', '#0B5345', '#0A3E23', '#117A65',
