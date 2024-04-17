8717723c921be008fa021285f295a422658d91e1
Onni-Petteri Rantanen <79081709+yoinkeds@users.noreply.github.com>
2024-04-17 19:54:30 +0300

    adding comments to code (#6)
    * random number generator
    
    adding random number generator
    
    * Capitalise letters, add more numbers
    
    * update randominumber.py
    
    adding a beautiful comment
    
    * update randominumber.py
    
    * update randominumber.py
    
    final comments

--- randominumber.py
+++ randominumber.py
@@ -1,9 +1,16 @@
+#Importing random module to generate random numbers.
 import random
 
+
+#creating list
 rand_list=[]
+#Defining number of randoms to generate
 n=10
+
+#Looping 'n' times to generate random numbers, then appending to list
 for i in range(n):
     rand_list.append(random.randint(2,2000))
 
+
+#Printing list
 print("Here is your list on numbers! ", rand_list)
-#look at how beautiful it is wow
